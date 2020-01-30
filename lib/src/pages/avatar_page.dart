8137020dc238a7d8e9326794e2514c2ee13690ab
Ismael Civera Pardo <icivera@AddingTechnology.local>
2020-01-30 12:00:00 +0100

    Se modifican las páginas de alert y avatar y se crea la nueva animated_container

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/9/9e/Stan_Lee_painting_by_abijith_ka_2.png'),
+              radius: 22.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2019/03/stan-lee-captain-marvel-cameo-mcu.jpg?fit=1080%2C608&quality=80&ssl=1'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
